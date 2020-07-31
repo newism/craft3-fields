049af369c0d90e8f35ffda6267f886841d8a3998
Sam Leighton <sam@newism.com.au>
2020-07-31 16:10:15 +1000

    fix bug with switching country when address is used on a non-entry

---
+++ src/controllers/AddressController.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * test plugin for Craft CMS 3.x
+ *
+ * test
+ *
+ * @link      http://newism.com.au
+ * @copyright Copyright (c) 2017 newism
+ */
+
+namespace newism\fields\controllers;
+
+use Craft;
+use craft\web\Controller;
+use Exception;
+use newism\fields\NsmFields;
+use Twig_Error_Loader;
+
+use yii\web\Response;
+use newism\fields\models\AddressModel;
+
+/**
+ * Address Controller
+ *
+ * Generally speaking, controllers are the middlemen between the front end of
+ * the CP/website and your plugin’s services. They contain action methods which
+ * handle individual tasks.
+ *
+ * A common pattern used throughout Craft involves a controller action gathering
+ * post data, saving it on a model, passing the model off to a service, and then
+ * responding to the request appropriately depending on the service method’s response.
+ *
+ * Action methods begin with the prefix “action”, followed by a description of what
+ * the method does (for example, actionSaveIngredient()).
+ *
+ * https://craftcms.com/docs/plugins/controllers
+ *
+ * @author    newism
+ * @package   Test
+ * @since     1.0.0
+ */
+class AddressController extends Controller
+{
+
+    // Protected Properties
+    // =========================================================================
+
+    /**
+     * @var    bool|array Allows anonymous access to this controller's actions.
+     *         The actions must be in 'kebab-case'
+     * @access protected
+     */
+    protected $allowAnonymous = [];
+
+    // Public Methods
+    // =========================================================================
+
+    /**
+     * @return response
+     * @throws Twig_Error_Loader
+     * @throws \yii\base\Exception
+     */
+    public function actionRefreshCountry(): Response
+    {
+        $this->requireAcceptsJson();
+
+        $address = Craft::$app->fields->getFieldByHandle('address');
+
+        $addressModel = new AddressModel([
+            'countryCode' => Craft::$app->request->post('countryCode')
+        ]);
+
+        $response = [
+            'html' => Craft::$app->getView()->namespaceInputs($address->renderFormFields($addressModel), Craft::$app->request->post('namespace'))
+        ];
+
+        return $this->asJson($response);
+    }
+
+}
